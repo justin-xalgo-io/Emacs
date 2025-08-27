eae5df7c97b29e388ad2b44840801f1345af6722
Justin Zoeller <justin@xalgo.io>
2025-08-27 12:35:09 +0200

    Updated config, added some functions.

--- init-settings.el
+++ init-settings.el
@@ -15,3 +15,20 @@
 ;; LaTeX settings & hooks.
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)  	; With AUCTeX LaTeX mode.
 (add-hook 'latex-mode-hook 'turn-on-reftex)   	; With Emacs LaTeX mode.
+
+;; Private methods.
+(defun insert-char-4 ()
+  "Read 4 keyboard inputs, interpret it as a hexadecimal number, and insert it as a character."
+  (interactive)
+  (let* ((k1 (read-key-sequence "____"))
+         (k2 (read-key-sequence (concat k1 "___")))
+         (k3 (read-key-sequence (concat k1 k2 "__")))
+         (k4 (read-key-sequence (concat k1 k2 k3 "_")))
+         (charcode (cl-parse-integer (concat k1 k2 k3 k4) :radix 16)))
+    (insert-char charcode)
+    (message (concat k1 k2 k3 k4 " => " (char-to-string charcode)))))
+
+(defun insert-v-bar ()
+  (interactive)
+  (insert-char (cl-parse-integer "007C" :radix 16)))
+
--- init.el
+++ init.el
@@ -7,11 +7,12 @@
 (use-package doom-themes
   :ensure t
   :custom
-	;; Global settings (defaults).
+
+  ;; Global settings (defaults).
   (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
   (doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
-	;; For treemacs users.
+  ;; For treemacs users.
   (doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
 
   :config
@@ -20,13 +21,13 @@
   ;; Enable flashing mode-line on errors.
   (doom-themes-visual-bell-config)
 
-	;; Enable custom neotree theme (nerd-icons must be installed!)
-	(doom-themes-neotree-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
 
   ;; or for treemacs users.
   (doom-themes-treemacs-config)
 
-	;; Corrects (and improves) org-mode's native fontification.
+  ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
 (custom-set-variables
